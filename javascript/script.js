0203913265179fed22f1b0c0fa8a6d157498cdd6
TopGek99 <a1767142@student.adelaide.edu.au>
2021-04-08 13:20:43 +0930

    added project 2 to portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,47 @@
-var aboutMeDiv = $("#about-me");
-var aboutMeLinks = $(".about-link");
-var portfolioDiv = $("#portfolio");
-var portfolioLinks = $(".portfolio-link");
-var contactDiv = $("#contact");
-var contactLinks = $(".contact-link");
-var dropdown = $("#navbarDropdownMenuLink");
+const aboutMeDiv = $('#about-me');
+const aboutMeLinks = $('.about-link');
+const portfolioDiv = $('#portfolio');
+const portfolioLinks = $('.portfolio-link');
+const contactDiv = $('#contact');
+const contactLinks = $('.contact-link');
+const dropdown = $('#navbarDropdownMenuLink');
+const title = $('title');
 
-
-aboutMeLinks.on("click", function(event) {
-    event.preventDefault();
-    portfolioDiv.css("display","none");
-    contactDiv.css("display","none");
-    aboutMeDiv.css("display","block");
-    portfolioLinks[0].classList.remove("active");
-    contactLinks[0].classList.remove("active");
-    aboutMeLinks[0].classList.add("active");
-    portfolioLinks[1].classList.remove("disabled");
-    contactLinks[1].classList.remove("disabled");
-    aboutMeLinks[1].classList.add("disabled");
-    dropdown.text("About Me");
+aboutMeLinks.on('click', function (event) {
+	event.preventDefault();
+	portfolioDiv.css('display', 'none');
+	contactDiv.css('display', 'none');
+	aboutMeDiv.css('display', 'block');
+	portfolioLinks[0].classList.remove('active');
+	contactLinks[0].classList.remove('active');
+	aboutMeLinks[0].classList.add('active');
+	portfolioLinks[1].classList.remove('disabled');
+	contactLinks[1].classList.remove('disabled');
+	aboutMeLinks[1].classList.add('disabled');
+	dropdown.text('About Me');
 });
-portfolioLinks.on("click", function(event) {
-    event.preventDefault();
-    aboutMeDiv.css("display","none");
-    contactDiv.css("display","none");
-    portfolioDiv.css("display","block");
-    aboutMeLinks[0].classList.remove("active");
-    contactLinks[0].classList.remove("active");
-    portfolioLinks[0].classList.add("active");
-    aboutMeLinks[1].classList.remove("disabled");
-    contactLinks[1].classList.remove("disabled");
-    portfolioLinks[1].classList.add("disabled");
-    dropdown.text("Portfolio");
+portfolioLinks.on('click', function (event) {
+	event.preventDefault();
+	aboutMeDiv.css('display', 'none');
+	contactDiv.css('display', 'none');
+	portfolioDiv.css('display', 'block');
+	aboutMeLinks[0].classList.remove('active');
+	contactLinks[0].classList.remove('active');
+	portfolioLinks[0].classList.add('active');
+	aboutMeLinks[1].classList.remove('disabled');
+	contactLinks[1].classList.remove('disabled');
+	portfolioLinks[1].classList.add('disabled');
+	dropdown.text('Portfolio');
 });
-contactLinks.on("click", function(event) {
-    aboutMeDiv.css("display","none");
-    portfolioDiv.css("display","none");
-    contactDiv.css("display","block");
-    aboutMeLinks[0].classList.remove("active");
-    portfolioLinks[0].classList.remove("active");
-    contactLinks[0].classList.add("active");
-    aboutMeLinks[1].classList.remove("disabled");
-    portfolioLinks[1].classList.remove("disabled");
-    contactLinks[1].classList.add("disabled");
-    dropdown.text("Contact");
+contactLinks.on('click', function (event) {
+	aboutMeDiv.css('display', 'none');
+	portfolioDiv.css('display', 'none');
+	contactDiv.css('display', 'block');
+	aboutMeLinks[0].classList.remove('active');
+	portfolioLinks[0].classList.remove('active');
+	contactLinks[0].classList.add('active');
+	aboutMeLinks[1].classList.remove('disabled');
+	portfolioLinks[1].classList.remove('disabled');
+	contactLinks[1].classList.add('disabled');
+	dropdown.text('Contact');
 });
-
-
-
